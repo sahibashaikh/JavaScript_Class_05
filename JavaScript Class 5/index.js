4f4362bdb9afb15acd2dbfa533e7494f5ed1ece6
sahibashaikh <121759560+sahibashaikh@users.noreply.github.com>
2023-04-02 11:17:22 +0500

    Add files via upload

---
+++ JavaScript Class 5/index.js
@@ -0,0 +1,60 @@
+//var firstname='Alexender';
+//var firstChar = firstname.slice(0,3)
+//console.log(firstChar)
+
+//var firstChar= firstname.charAt(3);
+//console.log(firstChar)
+//
+//var name= "sara"
+//var lastChar= name.charAt(name.length - 1);
+//
+//var text= "JavaScript is Fun!"
+//for (var i = 0; i < text.length; i++) {
+//     if (text.charAt(i) === "!") {
+//     alert("Exclamation point found!");
+//     break;
+//     }
+//     }
+
+
+//var text= "In previous chapters you learned two different ways to replace World War II "
+//     var firstChar = text.indexOf("World War II");
+//    console.log(firstChar);
+ //if (firstChar !== -1) {
+ //text = text.slice(0, firstChar) + "the Second World War" + text.slice(firstChar + 12);
+//console.log()
+//}
+
+//var newText = text.replace("World War II", "the Second World War");
+//console.log(newText)
+
+// globe method//
+
+//var text= "Java is awsome. java is fun";
+//var newText = text.replace(/java/g, "javascript");
+//console.log(newText);
+
+
+
+//var num= -22.05
+//console.log(num)
+//num = Math.ceil(num)
+//console.log=(num)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
